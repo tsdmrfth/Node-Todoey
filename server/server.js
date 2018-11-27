579b4bb484bdc7c58cb44e2d2f0d199ccb450654
tsdmrfth <line.939018>
2018-11-28 00:45:54 +0300

    Code refactoring.

--- server/server.js
+++ server/server.js
@@ -1,33 +1,3 @@
-const mongoose = require('mongoose')
-const { Todo } = require('../model/Todo')
-const { User } = require('../model/User')
-
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp')
-
-const newTodo = new Todo({
-    text: 'Add todos',
-    completed: false
-})
-
-/* newTodo.save()
-    .then(res => {
-        console.log('Todo saved: ', res)
-    })
-    .catch(err => {
-        console.log('Unable to add todo', err)
-    }) */
-
-const newUser = new User({
-    email: 'tsdmrfth@gmail.com',
-    password: 'superbum',
-    age: 22,
-})
-
-newUser.save()
-    .then(res => {
-        console.log('User saved: ', res)
-    })
-    .catch(err => {
-        console.log('Unable to add user', err)
-    })
+const { mongoose } = require('./db/mongoose')
+const { Todo } = require('./model/Todo')
+const { User } = require('./model/User')
