8bff1ff12dfe69fc3860b43e8719a78ed1be54f5
tsdmrfth <line.939018>
2018-11-29 01:31:54 +0300

    Added GET /todos/:id endpoint and it's test cases.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./model/Todo')
@@ -43,6 +44,27 @@ app.get('/todos', (req, res) => {
         .catch(err => {
             res.status(500).send('Could not fetch todos')
         })
+})
+
+app.get('/todos/:id', (req, res) => {
+    const { id } = req.params
+    if (id) {
+        const isValidId = ObjectID.isValid(id)
+        if (!isValidId) {
+            return res.status(404).send('Id is not valid')
+        }
+        Todo.findById(id).then(todo => {
+            if (!todo) {
+                return res.status(404).send({ message: 'Todo not found' })
+            }
+
+            res.send({ todo })
+        }).catch(err => {
+            res.status(400).send(err)
+        })
+    } else {
+        res.status(404).send('Id is required')
+    }
 })
 
 app.listen(3400, () => {
--- tests/server.test.js
+++ tests/server.test.js
@@ -1,5 +1,6 @@
 const expect = require('expect')
 const request = require('supertest')
+const { ObjectID } = require('mongodb')
 
 const { app } = require('./../server/server')
 const { Todo } = require('./../server/model/Todo')
@@ -34,6 +35,28 @@ describe('\nPOST /todos', () => {
             .send({ text })
             .expect(res => {
                 expect(res.body.text).toBe(text)
+            })
+            .end(done)
+    })
+
+})
+
+describe('GET /todos/:id', () => {
+
+    it('should return status code 404 if id is not a valid ObjectID', (done) => {
+        const id = '31283udsjfkads'
+        request(app)
+            .get(`/todos/${id}`)
+            .expect(404)
+            .end(done)
+    })
+
+    it('should return 404 with Todo not found message if no todo', (done) => {
+        const id = new ObjectID().toHexString()
+        request(app)
+            .get(`/todos/${id}`)
+            .expect(res => {
+                expect(res.body.message).toEqual('Todo not found')
             })
             .end(done)
     })
